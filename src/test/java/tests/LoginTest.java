7d770a3b7b2a5b738f9c1e48a283272069c06ebe
saifakh0604 <saifakh06@gmail.com>
2025-10-26 19:58:04 +0530

    move page folder from src/main/java -> src/test/java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,8 +1,6 @@
 package tests;
 
 import base.BaseTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
